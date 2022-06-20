f8aeb56ba5252739ba8889e1dbfcefc8712c01ae
IcePillow <poplizardo4321@gmail.com>
2022-06-20 14:01:19 +0800

    Added function that test playerSelection to computerSelection

--- main.js
+++ main.js
@@ -4,3 +4,37 @@ function computerPlay() {
     return CHOICE[Math.floor(Math.random() * CHOICE.length)];
 }
 
+function playRound(playerSelection, computerSelection) {
+    const rock = /^rock$/i; 
+    const paper = /^paper$/i;
+    const scissors = /^scissors/i;
+
+    if (rock.test(playerSelection)) {
+        if (computerSelection === "Scissors") {
+            return `You win! ${playerSelection} beats ${computerSelection}`;
+        }
+        else if (computerSelection === "Paper") {
+            return `You lose! ${computerSelection} beats ${playerSelection}`;
+        } else {
+            return "No one wins, No one loses"
+        }
+    } else if (paper.test(playerSelection)) {
+        if (computerSelection === "Rock") {
+            return `You win! ${playerSelection} beats ${computerSelection}`;
+        } else if (computerSelection === "Scissors") {
+            return `You lose! ${computerSelection} beats ${playerSelection}`;
+        } else {
+            return "No one wins, No one loses"
+        }
+    } else if (scissors.test(playerSelection)) {
+        if (computerSelection === "Paper") {
+            return `You win! ${playerSelection} beats ${computerSelection}`;
+        } else if (computerSelection === "Rock") {
+            return `You lose! ${computerSelection} beats ${playerSelection}`;
+        } else {
+            return "No one wins, No one loses"
+        }
+    } else {
+        return `${playerSelection} is not a valid input`
+    }
+} 
