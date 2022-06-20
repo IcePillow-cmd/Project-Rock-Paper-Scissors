4d6614946dbf164c5f97bf92a0cffa1a12e66a83
IcePillow <poplizardo4321@gmail.com>
2022-06-20 15:32:12 +0800

    Changed return to console.log in playRound function
    This is for the loop to efficiently setup the points system each round by using the return value as the basis of which to put the point on without affecting other functionalities except alert().

--- main.js
+++ main.js
@@ -11,28 +11,28 @@ function playRound(playerSelection, computerSelection) {
 
     if (rock.test(playerSelection)) {
         if (computerSelection === "Scissors") {
-            return `You win! ${playerSelection} beats ${computerSelection}`;
+            console.log( `You win! ${playerSelection} beats ${computerSelection}`);
         }
         else if (computerSelection === "Paper") {
-            return `You lose! ${computerSelection} beats ${playerSelection}`;
+            console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
         } else {
-            return "No one wins, No one loses"
+            console.log( "No one wins, No one loses");
         }
     } else if (paper.test(playerSelection)) {
         if (computerSelection === "Rock") {
-            return `You win! ${playerSelection} beats ${computerSelection}`;
+            console.log( `You win! ${playerSelection} beats ${computerSelection}`);
         } else if (computerSelection === "Scissors") {
-            return `You lose! ${computerSelection} beats ${playerSelection}`;
+            console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
         } else {
-            return "No one wins, No one loses"
+            console.log( "No one wins, No one loses");
         }
     } else if (scissors.test(playerSelection)) {
         if (computerSelection === "Paper") {
-            return `You win! ${playerSelection} beats ${computerSelection}`;
+            console.log( `You win! ${playerSelection} beats ${computerSelection}`);
         } else if (computerSelection === "Rock") {
-            return `You lose! ${computerSelection} beats ${playerSelection}`;
+            console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
         } else {
-            return "No one wins, No one loses"
+            console.log( "No one wins, No one loses");
         }
     } else {
         return `${playerSelection} is not a valid input`
@@ -44,7 +44,6 @@ function game() {
         let userInput = prompt("Rock, Paper or Scissors?");
         let computerInput = computerPlay();
         alert(playRound(userInput, computerInput));
-        console.log(playRound(userInput, computerInput));
     }
 }
 
