e50865fa173995ad97f6c422f59f4d35480f2708
IcePillow <poplizardo4321@gmail.com>
2022-06-20 18:37:35 +0800

    Added comments in the code

--- main.js
+++ main.js
@@ -5,13 +5,16 @@ function computerPlay() {
 }
 
 function playRound(playerSelection, computerSelection) {
+    // Regex variables
     const ROCK = /^rock$/i; 
     const PAPER = /^paper$/i;
     const SCISSORS = /^scissors/i;
+    //Message variables
     const WIN = `You win! ${playerSelection} beats ${computerSelection}\n\n`;
     const LOSE = `You lose! ${computerSelection} beats ${playerSelection}\n\n`;
     const TIE = `You and Computer both chose ${playerSelection}, It's a Tie\n\n`;
 
+    //Console logs for message and return for point system basis
     if (ROCK.test(playerSelection)) {
         if (computerSelection === "Scissors") {
             console.log(WIN);
@@ -53,11 +56,14 @@ function playRound(playerSelection, computerSelection) {
 } 
 
 function game() {
+    // Score variables are outside to prevent re-initialization
     let playerScore = 0;
     let computerScore = 0;
     for (let i = 0; i < 5; i++) {
+        // getting input from user and ocmputer
         let userInput = prompt("Rock, Paper or Scissors?");
         let computerInput = computerPlay();
+        // to show message in console
         console.log(`Round ${i}:\n`)
         let round = playRound(userInput, computerInput);
         alert(round);
